7c6a00b4c1e77b098f54a2006873c3d2fb22ff4a
Jakob Boghdady <58260820+jakoberpf@users.noreply.github.com>
2023-07-27 14:16:27 +0200

    fixes

--- sync/sync-modules.py
+++ sync/sync-modules.py
@@ -1,11 +1,12 @@
 # https://digitalvarys.com/git-operations-with-python-scripting/
+import contextlib
 import os
 import shutil
 from os import path
 from time import strftime, localtime
 
 from git import Repo
-from github import Auth, Github
+from github import Auth  # , Github
 
 repo_owner = "jakoberpf"
 
@@ -49,7 +50,8 @@ github_files = [
 
 sync_branch_name = "terraform-module-template-sync"
 
-github_token = os.getenv("PUBLIC_REPO_ACCESS_TOKEN")
+github_token = os.getenv("REPO_WORKFLOWS_TOKEN")
+
 
 # def clone_or_fetch_repository():
 
@@ -72,10 +74,10 @@ def create_pull_request(github, repo_owner, repo_name, base_branch, head_branch,
 
 def main():
   for repo_name in repo_names:
-    git_url = f"https://{github_token}@github.com/{repo_owner}/{repo_name}.git"
-    print(git_url)
+    git_url = f"https://{repo_owner}:{github_token}@github.com/{repo_owner}/{repo_name}.git"
+    print(f"{git_url}")
     repo_dir = path.join("repos/", repo_name)
-    print(repo_dir)
+    print(f"{repo_dir}")
 
     if path.isdir(repo_dir):
       print("repo already present, not cloning")
@@ -86,19 +88,19 @@ def main():
       print("repo cloned")
 
     repo.config_writer().set_value("name", "email", "Jakob Boghdady").release()
-    repo.config_writer().set_value("name", "email", "github@jakoberpf.de").release()
+    repo.config_writer().set_value("name", "email", "58260820+jakoberpf@users.noreply.github.com").release()
 
     os.system("git config --global user.name \"Jakob Boghdady\"")
-    os.system("git config --global user.email \"github@jakoberpf.de\"")
+    os.system("git config --global user.email \"58260820+jakoberpf@users.noreply.github.com\"")
 
     print(repo.remote().refs)
 
     if sync_branch_name in repo.remote().refs:
       print("branch already exists")
-      current = repo.git.checkout(sync_branch_name)
+      repo.git.checkout(sync_branch_name)
     else:
       print("branch does not exists")
-      current = repo.create_head(sync_branch_name).checkout()
+      repo.create_head(sync_branch_name).checkout()
 
     # update .GitHub files
 
@@ -107,13 +109,17 @@ def main():
       print("files removed")
 
     shutil.copytree(path.join('.github'), path.join(repo_dir, ".github"))
+
+    with contextlib.suppress(FileNotFoundError):
+      os.remove(repo_dir + '/.github/workflows/sync-modules.yaml')
+
     print('files updates')
 
     if repo.index.diff(None) or repo.untracked_files:
       repo.git.add(A=True)
       dtime = strftime('%d-%m-%Y %H:%M:%S', localtime())
-      repo.git.commit(m='Updated on' + dtime)
-      repo.git.push('--set-upstream', 'origin', current)
+      repo.git.commit(m='Updated on ' + dtime)
+      repo.git.push('--set-upstream', 'origin', sync_branch_name)
       print('git push')
 
     else:
@@ -121,7 +127,7 @@ def main():
 
     auth = Auth.Token(github_token)
 
-    github = Github(auth=auth)
+    # github = Github(auth=auth)
 
     title = "Sync from template module"
     body = "This pull request syncs this module with the template module."
